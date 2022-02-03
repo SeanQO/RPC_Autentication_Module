493bce85b1cb27aaeb076262f9e46e087d91dcd2
SeanQO <seanquintero3@gmail.com>
2022-02-03 01:00:40 -0500

    Added id as a parameter to edit delete and details urls

--- RPC_Autentication_Module/Models/user.cs
+++ RPC_Autentication_Module/Models/user.cs
@@ -1,23 +1,30 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RPC_Autentication_Module.Models
 {
     public class User
     {
         public int ID { get; set; }
+        
         [Required]
         [StringLength(30, MinimumLength = 3)]
         public String Username { get; set; }
+        
         [Required]
         [StringLength(30, MinimumLength = 3)]
         public String Password { get; set; }
+        
         [Required]
         [StringLength(30, MinimumLength = 1)]
         public String Firstname { get; set; }
+
         [Required]
         [StringLength(30, MinimumLength = 1)]
         public String Lastname { get; set; }
+
+        [Display(Name = "Birth Date")]
         [Required]
         [DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
