b0a49c7f32ae53f52dc7c1b1603a3ce58a247bb0
SeanQO <seanquintero3@gmail.com>
2022-02-06 02:47:38 -0500

    created logout

---
+++ RPC_Autentication_Module/Pages/Users/Logout.cshtml.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using RPC_Autentication_Module.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace RPC_Autentication_Module.Pages.Users
+{
+    public class LogoutModel : PageModel
+    {
+        public async Task<IActionResult> OnPostAsync() 
+        {
+            await HttpContext.SignOutAsync("CookieAuth");
+            return RedirectToPage("Login");
+        }
+    }
+}
