d25a9aa98617b83feacf96c7b64108bcf438674d
SeanQO <seanquintero3@gmail.com>
2022-02-05 20:05:14 -0500

    creaded login pages, removed old main page, aded links between user pages and new login pages

--- RPC_Autentication_Module/Pages/Users/Login.cshtml.cs
+++ RPC_Autentication_Module/Pages/Users/Login.cshtml.cs
@@ -1,17 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RPC_Autentication_Module.Pages.Users
 {
     public class LoginModel : PageModel
     {
-        public void OnGet() 
-        { 
-        
+        private readonly ILogger<LoginModel> _logger;
+
+        public LoginModel(ILogger<LoginModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
         }
     }
 }
